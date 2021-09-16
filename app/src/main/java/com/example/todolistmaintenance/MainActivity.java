2e56edfd6529144397eaf3b4b7499c97e8a44740
StephnyDebra <stephnydebra3@gmail.com>
2021-09-16 19:09:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todolistmaintenance/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.todolistmaintenance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+    private Object Activity_2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openActivity_3(); }
+        });
+    }
+    public void openActivity_3(){
+        Intent intent=new Intent (this, Activity_3.class);
+        startActivity(intent);
+    };
+}
